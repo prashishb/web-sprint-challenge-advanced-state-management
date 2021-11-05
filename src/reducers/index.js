870d6aefe41061641ba406c5079af9cad0d30ca9
prashishb <prashb.web@gmail.com>
2021-11-05 09:28:07 -0400

    completed reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,55 @@
+import {
+  FETCH_START,
+  FETCH_SUCCESS,
+  FETCH_ERROR,
+  ADD_SMURF,
+  SET_ERROR,
+} from '../actions';
 
 export const initialState = {
-}
+  smurfs: [],
+  isLoading: false,
+  error: '',
+};
 
-const reducer = ()=>{
-}
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case FETCH_START:
+      return {
+        ...state,
+        smurfs: [],
+        isLoading: true,
+        error: '',
+      };
+    case FETCH_SUCCESS:
+      return {
+        ...state,
+        smurfs: action.payload,
+        isLoading: false,
+        error: '',
+      };
+    case FETCH_ERROR:
+      return {
+        ...state,
+        smurfs: [],
+        error: action.payload,
+        isLoading: false,
+      };
+    case ADD_SMURF:
+      return {
+        ...state,
+        smurfs: [...state.smurfs, action.payload],
+        error: '',
+      };
+    case SET_ERROR:
+      return {
+        ...state,
+        error: action.payload,
+      };
+    default:
+      return state;
+  }
+};
 
 //**************DO NOT EDIT ANY CODE BEYOND THIS POINT**************//
 export default reducer;
